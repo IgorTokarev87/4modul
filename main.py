5b10bb30291eb24826bfd858621e42f8512f350c
Игорь <tokarev-igorek07@mail.ru>
2023-08-19 20:29:00 +0500

    Первый

---
+++ main.py
@@ -0,0 +1,7 @@
+def strocounter(s):# 0(N)
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+    for sym, count in syms_counter.items():
+        print(sym, count)
+strocounter('adgfaaa')
